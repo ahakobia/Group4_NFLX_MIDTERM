d5407b10c166153d093ee21b6c38375170eba5f1
Justin Baytosh <justin.baytosh@gmail.com>
2022-10-05 16:50:14 -0700

    fixed sql table issue, added line graph

--- Justin/Justin_Queries.sql
+++ Justin/Justin_Queries.sql
@@ -1,6 +1,6 @@
 DROP TABLE us_energy;
 
-CREATE TABLE us_energy (
+CREATE TABLE us_energy_2 (
 	id SERIAL PRIMARY KEY,
 	row INT,
 	year INT,
@@ -11,16 +11,18 @@ CREATE TABLE us_energy (
 	energy FLOAT
 );
 
-COPY us_energy (row, year, month, state, producer, source, energy) FROM '/Users/justinbaytosh/Desktop/coding/netflix/advanced/midterm-project/Resources/organised_Gen.csv'
+COPY us_energy_2 (row, year, month, state, producer, source, energy) FROM '/Users/justinbaytosh/Desktop/coding/netflix/advanced/midterm-project/Resources/organised_Gen.csv'
 DELIMITER ','
 CSV HEADER;
 
-CREATE VIEW renewable_energy AS
-SELECT year, producer, SUM(energy) AS total_generated FROM us_energy
-WHERE year >= 2011 AND year < 2022 AND (source='Wind' OR source='Solar Thermal and Photovoltaic' OR source='Hydroelectric Conventional' OR source='Geothermal' OR source='Wood and Wood Derived Fuels' OR source='Other Biomass' OR source='Pumped Storage')
+CREATE VIEW renewable_energy_2 AS
+SELECT year, producer, SUM(energy) AS total_generated FROM us_energy_2
+WHERE year >= 2011 AND year < 2022 
+AND (source='Wind' OR source='Solar Thermal and Photovoltaic' OR source='Hydroelectric Conventional' OR source='Geothermal' OR source='Wood and Wood Derived Fuels' OR source='Other Biomass' OR source='Pumped Storage')
+AND (state != 'US-TOTAL' AND producer != 'Total Electric Power Industry' AND source != 'Total')
 GROUP BY year, producer
 ORDER BY year DESC, producer DESC;
 
-COPY (SELECT * FROM renewable_energy) TO '/Users/justinbaytosh/Desktop/coding/netflix/advanced/midterm-project/Justin/renewable_energy.csv' 
+COPY (SELECT * FROM renewable_energy_2) TO '/Users/justinbaytosh/Desktop/coding/netflix/advanced/midterm-project/Justin/renewable_energy.csv' 
 DELIMITER ',' 
 CSV HEADER;
